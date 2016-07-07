c4074e018e77ef27acc751c5eaf281660410e278
Cort Ferguson <98cort@gmail.com>
2016-07-07 20:39:14 +0000

    fixing minor error to get sections to stay after refreshing

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -14,14 +14,14 @@ class Instructor::SectionsController < ApplicationController
   end
   
   def update
-    current_section.update_attributes(lesson_params)
+    current_section.update_attributes(section_params)
     render text: 'updated!'
   end
   
   private
   
   def require_authorized_for_current_section
-    if current_course.user != current_user
+    if current_section.course.user != current_user
       return render text: 'Unauthorized', status: :unauthorized
     end
   end
@@ -30,6 +30,10 @@ class Instructor::SectionsController < ApplicationController
     if current_course.user != current_user
       render text: "Unauthorized", status: :unauthorized
     end
+  end
+  
+  def current_section
+    @current_section ||= Section.find(params[:id])
   end
   
   helper_method :current_course
