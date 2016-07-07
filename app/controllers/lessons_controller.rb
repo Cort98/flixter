1fc175857d782fb9dc8aa5e881b1ad7a2db5bd2e
Cort Ferguson <98cort@gmail.com>
2016-07-07 05:28:01 +0000

    lesson permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson
   def show
     
   end
   
   private
+  
+  def require_authorized_for_current_lesson
+    unless current_user.enrolled_in?(current_lesson.section.course) or (current_lesson.section.course.user == current_user)
+      redirect_to course_path(current_lesson.section.course), alert: 'You do not have access to that lesson'
+    end
+  end
   
   helper_method :current_lesson
   def current_lesson
