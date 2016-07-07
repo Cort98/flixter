6c731baa967fd3550ed8f398b2837ec2f36d27ef
Cort Ferguson <98cort@gmail.com>
2016-07-07 01:23:03 +0000

    Course details and lesson details

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,13 @@ Flixter::Application.routes.draw do
   root 'static_pages#index'
   
   resources :courses, only: [:index, :show]
-  
+  resources :lessons, only: [:show]
   namespace :instructor do
     resources :sections, only: [] do
       resources :lessons, only:[:new, :create]
     end
     resources :courses, only: [:new,:create,:show] do
-      resources :sections, only: [:new, :create] 
+      resources :sections, only: [:new, :create]
     end
   end
   
